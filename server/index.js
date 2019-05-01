cd152b33d54b449896f404e7a52f6a15d12e5bdf
obi chinedu frank <frankieetchy@gmail.com>
2019-05-01 11:53:05 +0100

    update index.js with morgan and consola fields

--- server/index.js
+++ server/index.js
@@ -1,12 +1,18 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const morgan = require('morgan');
+const consola = require('consola');
 
 dotenv.config();
 const app = express();
+
+app.use(morgan('combined'));
 
 app.get('/', (req, res) => {
   res.send({ message: 'Hello Books Deferral' });
 });
 
 const { PORT } = process.env;
-app.listen(PORT);
+app.listen(PORT, () => {
+  consola.success(`server start at port ${PORT}`);
+});
